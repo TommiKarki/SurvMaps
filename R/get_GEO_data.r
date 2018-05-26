06c49924a098543d8ab13c3878358cf79d4bb2d7
TommiKarki <27780557+TommiKarki@users.noreply.github.com>
2018-05-26 10:50:52 +0200

    Add files via upload

--- R/get_GEO_data.r
+++ R/get_GEO_data.r
@@ -1,15 +1,16 @@
 #' Get GEO data from ECDC GIS server
 #'
-#' This function gets the geodata from ECDC GIS server into R SpatialpolygonsDataFrame or SpatialpointsDataFrame. 
-#' Gives a warning when closing the connection.
+#' This function gets the geodata from ECDC GIS server into R SpatialpolygonsDataFrame or SpatialpointsDataFrame.
 #' @param layer Select the layer for Hybrid Layer service: 0 for Points, 1 for Polygons.
-#' @param STAT_LEVL Which data level; 0 for EU/EEA and candidate country level, 10 for the rest of world country level. 
-#' 1,2,3 = NUTS levels; 11,12 = GAUL levels; 21,22,23,24 = GADM levels. Defaults to 0 for EU/EEA. Give a single value or vector.
-#' @param FIELDS = Which "additional" field to retrieve from the hybrid layer, defaults to "GEO_ID", for additional
-#' FIELDS to retrieve from the hybrid layer give a character vector of field names. Field names available from:
-#' https://gis.ecdc.europa.eu/public/rest/services/UtilitiesServices/EMMA_GoMap_GC_HL_V1_3_WebMerc/MapServer/
+#' @param STAT_LEVL Which data level to choose; see details. Defaults to 0 for EU/EEA. Give a single value or vector.
+#' @param FIELDS Which data fields to retrieve from the hybrid layer; see details. Defaults to "GEO_ID".
+#' Give a single value or character vector of relevant field names.
 #' @param isValid isValid=1 removes the obsolete NUTS regions.
-#' @author Tommi Karki.
+#' @details STAT_LEVL; 0 for EU/EEA and candidate country level, 10 for the rest of world country level. 
+#' 1,2,3 = NUTS levels; 11,12 = GAUL levels; 21,22,23,24 = GADM levels. 
+#' FIELDS; for additional fields to retrieve from the hybrid lay, see:
+#' https://gis.ecdc.europa.eu/public/rest/services/UtilitiesServices/EMMA_GoMap_GC_HL_V1_3_WebMerc/MapServer/
+#' @author Tommi Karki, with thanks to Silviu Ionescu.
 #' @export
 #' @examples
 #' # get EU/EEA country polygons
@@ -44,7 +45,7 @@ url_full <- modify_url(url_ecdcGisSrv,
                        path = list(url_srvcLocation, url_layer, "query"), 
                        query = list(where = url_whereClause, outFields=url_outFields,
                                     f= "geojson"))
-gjson_data <- geojson_sp(geo_pretty(as.geojson(Dump_From_GeoJson(url_full))))
+suppressWarnings(gjson_data <- geojson_sp(geo_pretty(as.geojson(Dump_From_GeoJson(url_full)))))
 
 return(gjson_data)
 }
